199cd53310f7f9936a3883a3ecc1e84d01660a95
Surekha <kolekar.surekha@gmail.com>
2014-06-16 10:48:37 +0000

    test cases changes done

--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
   include UsersHelper
-  test "should return the blank email address for the nil username passed" do
+  test "should return the empty email address for the blank username passed" do
     email = show_user_email('')
     assert_nil email, nil   
   end
   
-  test "should return the eamil address for the username passed" do
+  test "should return the email address for the username passed" do
     email = show_user_email('surekhakolekar')
     assert_equal email, "kolekar.surekha@gmail.com"
     
